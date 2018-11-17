61b9b0bf21d2c39d8a8b2e2c5773aedeea358c32
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-11-17 19:10:45 +0700

    Fix Navi

--- Website/JSCommon.js
+++ Website/JSCommon.js
@@ -1,14 +1,13 @@
 ﻿ $(function(){
 
 })
- /* Set the width of the side navigation to 250px */
+ /* Set the width of the side navigation to 1000px */
 function openNav() {
-     document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("myPageTitle").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
- 
