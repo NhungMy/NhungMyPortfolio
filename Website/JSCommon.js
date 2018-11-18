140e00ddcd492f1eef477d1fa912dc46599585ed
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-11-18 09:41:03 +0700

    Update Home Navi

--- Website/JSCommon.js
+++ Website/JSCommon.js
@@ -1,13 +1,19 @@
-﻿ $(function(){
+/* Set the width of the side navigation to 1000px */
+function openNav(x) {
+  var width = window.innerWidth;
 
-})
- /* Set the width of the side navigation to 1000px */
-function openNav() {
+  if (width <= 700) {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.height = "50%";
+  } else {
     document.getElementById("mySidenav").style.width = "50%";
-    document.getElementById("myPageTitle").style.display = "none";
+    document.getElementById("mySidenav").style.height = "100%";
+  }
+  
+  document.getElementById("myPageTitle").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
