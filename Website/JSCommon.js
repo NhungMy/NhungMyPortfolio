ed00800e92badde8d4134fa4f17a483b65b32c61
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-11-19 20:33:06 +0700

    Fix Navi

--- Website/JSCommon.js
+++ Website/JSCommon.js
@@ -1,5 +1,10 @@
+var isShowSideBar = false;
+
 /* Set the width of the side navigation to 1000px */
-function openNav(x) {
+function openNav() {
+  isShowSideBar = true;
+  document.getElementById("mySidenavItem").style.visibility = "visible";
+
   var width = window.innerWidth;
 
   if (width <= 700) {
@@ -9,11 +14,17 @@ function openNav(x) {
     document.getElementById("mySidenav").style.width = "50%";
     document.getElementById("mySidenav").style.height = "100%";
   }
-  
+
   document.getElementById("myPageTitle").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "120px";
 }
+
+window.addEventListener('resize', function(event){
+  if (isShowSideBar) {
+    openNav();
+  }
+});
