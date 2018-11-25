c41d3020d5077dd9bacc915e207b80601ff497a7
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-11-25 22:02:15 +0700

    Fix naviation bar selected

--- Website/JSCommon.js
+++ Website/JSCommon.js
@@ -1,44 +1,17 @@
-// var isShowSideBar = false;
-
-// /* Set the width of the side navigation to 1000px */
-// function openNav() {
-//   isShowSideBar = true;
-//   document.getElementById("mySidenavItem").style.visibility = "visible";
-
-//   var width = window.innerWidth;
-
-//   if (width <= 700) {
-//     document.getElementById("mySidenav").style.width = "100%";
-//     document.getElementById("mySidenav").style.height = "50%";
-//   } else {
-//     document.getElementById("mySidenav").style.width = "50%";
-//     document.getElementById("mySidenav").style.height = "100%";
-//   }
-
-//   document.getElementById("myPageTitle").style.display = "none";
-// }
-
-// /* Set the width of the side navigation to 0 */
-// function closeNav() {
-//   document.getElementById("mySidenav").style.width = "120px";
-// }
-
-// window.addEventListener('resize', function(event){
-//   if (isShowSideBar) {
-//     openNav();
-//   }
-// });
 var isShowSideBar = false;
+
 $(document).ready(function() {
   $("#myButtonNavi").click(function() {
     if (isShowSideBar) {
       $("#mySidenav").removeClass("open");
       $(".img-action-menu").attr("src", "image/menu.png");
       isShowSideBar = false;
+      document.getElementById("myPageTitle").style.display = "block";
     } else {
       $("#mySidenav").addClass("open");
       $(".img-action-menu").attr("src", "image/arrow-menu.png");
       isShowSideBar = true;
+      document.getElementById("myPageTitle").style.display = 'none';
     }
   });
 
@@ -51,6 +24,7 @@ $(document).ready(function() {
       $("#mySidenav").removeClass("open");
       $(".img-action-menu").attr("src", "image/menu.png");
       isShowSideBar = false;
+      document.getElementById("myPageTitle").style.display = "block";
     }
   });
 });
