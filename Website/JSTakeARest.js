70482d62aba76fdd5c933179872a22ee0b807530
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-12-01 16:07:45 +0700

    Fix Navi - take a rest - mywork

--- Website/JSTakeARest.js
+++ Website/JSTakeARest.js
@@ -1,15 +1,24 @@
-var isShowGridItem = true;
+var isShowGridListItem = false;
 
 $(document).ready(function() {
-   
     var gridButton = document.getElementById("myGridButton");
+
     gridButton.addEventListener('click', function(e) {
-        if (isShowGridItem) {
-            $("#myGridButton").attr("src", "image/menu.png");
-            isShowGridItem = false;
+        if (isShowGridListItem) {
+            $("#myGridButton").attr("src", "image/Grid-window-ic.svg");
+            var windowView = document.getElementById("window-container");
+            var listView = document.getElementById("list-container");
+            windowView.style.display = "none";
+            listView.style.display = "block";
+            isShowGridListItem = false;
         } else {
-            $("#myGridButton").attr("src", "image/GridButton.png");
-            isShowGridItem = true;
+            console.log("Click me");
+            $("#myGridButton").attr("src", "image/Grid-list-ic.svg");
+            var windowView = document.getElementById("window-container");
+            var listView = document.getElementById("list-container");
+            windowView.style.display = "block";
+            listView.style.display = "none";
+            isShowGridListItem = true;
         }
     });
 
