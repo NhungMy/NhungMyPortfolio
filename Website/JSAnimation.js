5317e0f7dbf819966cfb077f54c38df0e9f5dcf8
TrieuNguyen <nguyendinhtrieu1996@gmail.com>
2018-12-01 17:57:05 +0700

    Add BodyMovin

---
+++ Website/JSAnimation.js
@@ -0,0 +1,13 @@
+  
+  /*------------- Animation Home -----------------*/
+  
+  /*------------- Animation About Me -----------------*/
+  bodymovin.loadAnimation({
+    container: document.getElementById('about-me-animation'),
+    path: 'animation/about.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true
+  })
+
+  /*------------- Animation Take A Rest -----------------*/
