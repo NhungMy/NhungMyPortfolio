8bb9bb62b1049cb3c41377bd97ece9fa3a937954
Nhung My <nhungmy162@gmail.com>
2018-12-16 20:36:15 +0700

    fix pagetitle

--- Website/JSMyWork.js
+++ Website/JSMyWork.js
@@ -6,7 +6,6 @@ function openyolo(){
 };
 
 function closeyolo(){
-	console.log("click")
 	var projectyolo = document.getElementById('project-yolo');
 	var closeyolo = document.getElementById('project-yolo');
 	projectyolo.style.display = "none";
